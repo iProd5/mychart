fee05b60ce1e466b5543c2abcc2c05e2e015e134
iProd5 <franklinluther3@gmail.com>
2019-12-18 14:28:41 +0000

    not working, fix it with a new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MyBarChartComponent } from "./my-bar-chart/my-bar-chart.component";
+import { ChartsModule } from "ng2-charts";
 
+const routes: Routes = [
+  { path: "my-bar-chart", component: MyBarChartComponent },
+  { path: "", component: MyBarChartComponent }
 
-const routes: Routes = [];
+  // { path: "bar-chart", component: MyBarChartComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ChartsModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
